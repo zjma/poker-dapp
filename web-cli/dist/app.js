101315f308a52006814c8b62ce8c7ff5521d8fb5
zhoujun.ma <zhoujun@aptoslabs.com>
2025-05-27 09:06:52 -0700

    delete webcli

--- web-cli/dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-document.addEventListener('DOMContentLoaded', () => {
-    const app = document.getElementById('app');
-    if (app) {
-        app.innerHTML = '<h1>Hello World</h1>';
-    }
-});
